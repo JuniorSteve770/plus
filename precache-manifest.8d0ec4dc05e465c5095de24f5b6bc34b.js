d028b2f16086647ecdf8d6edbfb2a03abaa2e9ab
Your Name <juniorsteve770@gmail.com>
2019-07-22 21:01:50 +0000

    Updates

---
+++ precache-manifest.8d0ec4dc05e465c5095de24f5b6bc34b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e026c12ae54c9dc052966e546642e0b0",
+    "url": "/plus/index.html"
+  },
+  {
+    "revision": "537c2fe5781634150305",
+    "url": "/plus/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/plus/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "537c2fe5781634150305",
+    "url": "/plus/static/js/main.f8529973.chunk.js"
+  },
+  {
+    "revision": "726851b54b703f788b45",
+    "url": "/plus/static/js/runtime~main.b9b13eb1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/plus/static/media/logo.5d5d9eef.svg"
+  }
+]);
